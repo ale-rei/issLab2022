2c0bce61090ca505e85d4774fffb3cc63c48afe1
Alessio Reitano <alessioreitano@MacBook-Air-di-Alessio.local>
2022-05-02 23:14:56 +0200

    robotCleaner

--- unibo.actor22/src/unibo/actor22/annotations/Actors22.java
+++
@@ -1,9 +0,0 @@
-package unibo.actor22.annotations;
-
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-
-@Retention(RetentionPolicy.RUNTIME)
-public @interface Actors22 {
-    Actor22[] value();
-}
--- unibo.actor22/src/unibo/actor22/annotations/Contexts22.java
+++
@@ -1,9 +0,0 @@
-package unibo.actor22.annotations;
-
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-
-@Retention(RetentionPolicy.RUNTIME)
-public @interface Contexts22 {
-    Context22[] value();
-}
